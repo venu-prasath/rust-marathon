e36e115e12f33e77410073132c9c7760f9951572
venu-prasath <venuprasth133@gmail.com>
2024-09-11 14:47:10 -0400

    Vectors collection explored

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ fn main() {
 
     read_from_file();
     using_chrono();
+    using_vectors();
 }
 
 fn is_even(num: i32) -> bool {
@@ -142,4 +143,23 @@ fn using_chrono() {
 
     let local = Local::now();
     println!("Current local time is: {}", local);
+}
+
+fn using_vectors() {
+    let vec = vec![1,2,3,4]; //Vec::new();
+    //vec.push(1);
+    //vec.push(2);
+
+    println!("{:?}", even_filter(&vec));
+    println!("{:?}", vec);
+}
+
+fn even_filter(vec: &Vec<i32>) -> Vec<i32> {
+    let mut new_vec = Vec::new();
+    for val in vec {
+        if val % 2 == 0 {
+            new_vec.push(*val);
+        }
+    }
+    return new_vec;
 }
